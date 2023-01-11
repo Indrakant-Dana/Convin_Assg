daa5e3fb35943a5fa58d83d624e45826f78ffea6
Indrakant Dana <60315832+Indrakant-Dana@users.noreply.github.com>
2023-01-11 19:41:53 +0530

    Setup React-Project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import react from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      Hello World
+    </div>
+  );
+}
+
+export default App;
